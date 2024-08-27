73b1d52ae500588bfd7d5706ba95529d95b98348
tj-commits <thefoxjones@gmail.com>
2024-08-27 10:20:10 -0500

    add cli

---
+++ cli/cli.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const f = require('../index')
+
+const myFalseValue = f.False()
+
+try {
+  if (f.isFalse(myFalseValue)) {
+    console.log(myFalseValue)
+    process.exit(0)
+  } else {
+    throw new Error('false is not false')
+  }
+} catch (err) {
+  console.log(err.message)
+  console.log(err.stack)
+  process.exit(1)
+}
